2d42385fde69c260a8e4e7e1917cc7a71250e9cd
Bruno Silva <brunosilvinosilva@gmail.com>
2020-01-27 15:18:58 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -99,6 +99,8 @@ def movies_with_directors_set(source)
   director_movies = dir_info_hash[:movies] 
   movies_by_dir << movies_with_director_key(director_name, director_movies) 
   i += 1 
+end 
+movies_by_dir
   
 end 
   # GOAL: For each director, find their :movies Array and stick it in a new Array
