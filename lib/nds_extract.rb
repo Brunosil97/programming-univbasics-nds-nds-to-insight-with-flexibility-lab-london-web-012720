1b87b8a7ddd36c51e5125155bec2ed33e70b0107
Bruno Silva <brunosilvinosilva@gmail.com>
2020-01-27 15:07:37 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,6 @@
 # Provided, don't edit
 require 'directors_database'
+require "pry"
 
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
@@ -34,6 +35,16 @@ end
 # Your code after this point
 
 def movies_with_director_key(name, movies_collection)
+  collection = []
+  index = 0 
+  while index < movies_collection.length do 
+  movie_data = movies_collection[index]
+  collection << movie_with_director_name(name, movie_data)
+  index += 1 
+end 
+collection
+end 
+  
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
   # of movies and a directors name to the movie_with_director_name method
   # and accumulate the returned Array of movies into a new Array that's
@@ -48,10 +59,25 @@ def movies_with_director_key(name, movies_collection)
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
-end
 
 
 def gross_per_studio(collection)
+  result = {}
+  i = 0 
+  
+  while i < collection.length do 
+    movie = collection[i] 
+    
+   if !result[movie[:studio]]
+     result[movie[:studio]] = movie[:worldwide_gross]
+   else 
+     result[movie[:studio]] += movie[:worldwide_gross]
+   end 
+    i += 1 
+  end 
+  result
+  end 
+  
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
   # each studio.
@@ -63,7 +89,7 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
-end
+
 
 def movies_with_directors_set(source)
   # GOAL: For each director, find their :movies Array and stick it in a new Array
